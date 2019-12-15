65272de755afe31badcfa1348d80591f239f5db4
sunkexing <1633896174@qq.com>
2019-12-15 12:43:08 +0800

    测试

---
+++ 4/4/4.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	for (a = 1000; a < 2000; a++)
+	{
+		
+		if (a%4 == 0)
+		
+			printf("%d\n", a);
+
+
+
+	}
+
+
+
+	return 0;
+}
